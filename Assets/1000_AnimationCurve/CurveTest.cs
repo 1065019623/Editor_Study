4955238766a3301e75e78bad2e8dd4081568cf60
1065019623 <1065019623@QQ.COM>
2017-09-02 19:29:50 +0800

    nothing

---
+++ Assets/1000_AnimationCurve/CurveTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurveTest : MonoBehaviour 
+{
+    public AnimationCurve m_animationCurve;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.position = new Vector3(Time.time, m_animationCurve.Evaluate(Time.time), transform.position.z);
+        Debug.Log(transform.position + " " + Time.time);
+	}
+}
